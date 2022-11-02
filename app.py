c001251746b38747d90989f1bee67883c330936d
Juho Siitonen <juho_tuomas@msn.com>
2022-11-02 16:32:13 +0200

    Created working testpage and opened db connection

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, request, render_templates
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/zwolt'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
-    return ""
+    return render_template("index.html")
+
+@app.route("/front", methods=["POST"])
+def front():
+    return render_template("front.html", name=request.form["name"])
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
